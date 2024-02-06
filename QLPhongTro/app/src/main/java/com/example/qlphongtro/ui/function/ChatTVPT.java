c46f086fe95da6cb114f8dd2a3d8bcb7187b6bb8
pangansr <phamthanhliem011203@gmail.com>
2024-02-06 21:58:15 +0700

    firebase

---
+++ QLPhongTro/app/src/main/java/com/example/qlphongtro/ui/function/ChatTVPT.java
@@ -0,0 +1,16 @@
+package com.example.qlphongtro.ui.function;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.qlphongtro.R;
+
+public class ChatTVPT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_tvpt);
+    }
+}
